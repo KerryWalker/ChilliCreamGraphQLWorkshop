723b6fb210ad1086268c7017c06648c7d769bc10
kerry walker <kerrywalker@kerrys-MacBook-Pro.local>
2023-06-13 21:04:19 +0100

    Session 1

---
+++ GraphQL/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConferencePlanner.GraphQL;
+using ConferencePlanner.GraphQL.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>();
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+});
+
+app.Run();
+
